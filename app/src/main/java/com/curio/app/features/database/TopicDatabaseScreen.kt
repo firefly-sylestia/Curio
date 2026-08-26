fd99bc48b93a9efd391763a8ca5c6be9bc53a1ba
kiarajugnu-stack <292969820+kiarajugnu-stack@users.noreply.github.com>
2026-08-26 16:01:52 +0000

    fix: add missing Spacer import in TopicDatabaseScreen page picker

--- app/src/main/java/com/curio/app/features/database/TopicDatabaseScreen.kt
+++ app/src/main/java/com/curio/app/features/database/TopicDatabaseScreen.kt
@@ -20,6 +20,7 @@ import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
